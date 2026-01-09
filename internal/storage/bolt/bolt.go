de86be28daa9b4844961199652cd00a78e68df71
Nelson Chidi (Nelmatrix743) <nelmatrix155@gmail.com>
2026-01-09 19:00:44 +0000

    feat(db): implement function for db initialization

--- internal/storage/bolt/bolt.go
+++ internal/storage/bolt/bolt.go
@@ -2,10 +2,28 @@ package bolt
 
 import "go.etcd.io/bbolt"
 
+var tasksBucket = []byte("tasks")
+
 type BoltDBStorage struct {
 	db *bbolt.DB
 }
 
 func NewBoltDBStorage(dbPath string) (*BoltDBStorage, error) {
-	return nil, nil
+	db, err := bbolt.Open(dbPath, 0o600, nil)
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Update(func(tx *bbolt.Tx) error {
+		_, err := tx.CreateBucketIfNotExists(tasksBucket)
+		return err
+	})
+
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return &BoltDBStorage{db: db}, nil
 }
